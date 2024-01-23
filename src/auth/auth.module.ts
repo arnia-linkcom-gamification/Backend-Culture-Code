3aeacb9e6b1b78c035eb3a090c954bb7e0f54b80
Orion Alves <oriondecarvalho@gmail.com>
2024-01-23 17:47:57 -0300

    fix: Import User in to AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { jwtOptions } from './jwt/jwt.options';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
-  imports: [JwtModule.registerAsync({ ...jwtOptions, global: true })],
+  imports: [
+    JwtModule.registerAsync({ ...jwtOptions, global: true }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
