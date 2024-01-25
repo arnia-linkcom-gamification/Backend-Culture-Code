26918c3eda9b51bbb3b57107f00627dda8601f46
Orion Alves <oriondecarvalho@gmail.com>
2024-01-24 22:44:44 -0300

    feature: create roles-guard

---
+++ src/auth/guards/roles-guard.ts
@@ -0,0 +1,25 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from 'src/decorators/role.decorator';
+import { RoleEnum } from 'src/enums/role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles: RoleEnum[] = this.reflector.get(
+      ROLES_KEY,
+      context.getHandler(),
+    );
+
+    if (!roles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    return !!roles.find((role: RoleEnum) => role == user.role);
+  }
+}
