08710f0a77727f6922712d73b417ee83652e1204
Glasielle <116851717+GlasiCR@users.noreply.github.com>
2024-01-30 19:51:52 -0300

    Documentation create jewel (#81)
    * include documentation to create jewel
    
    * fix: service -> create jewel

---
+++ src/jewels/docs/response-create-jewel.doc.ts
@@ -0,0 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { JewelTypeEnum } from 'src/enums/jewel-type.enum';
+export class ResponseCreateJewelDoc {
+  @ApiProperty({
+    enum: JewelTypeEnum,
+    description: 'Refere-se ao nome da joia cadastrada.',
+    example: JewelTypeEnum.alma,
+  })
+  type: JewelTypeEnum;
+
+  @ApiProperty({
+    type: String,
+    description: 'Descrição sobre a joia cadastrada.',
+    example: 'A joia da alma permite acessar a essência de cada indivíduo, ...',
+  })
+  habilities: string;
+
+  @ApiProperty({
+    type: null,
+    description: 'É o caminho da imagem referente a joia cadastrada',
+    example: null,
+  })
+  image: null;
+
+  @ApiProperty({
+    type: Number,
+    description: 'É a chave única da joia no banco de dados',
+    example: 7,
+  })
+  id: number;
+
+  @ApiProperty({
+    type: Date,
+    description: 'Data de inclusão do objeto no banco de dados',
+    example: '2024-01-24T23:00:56.481Z',
+  })
+  createAt: Date;
+
+  @ApiProperty({
+    type: Date,
+    description: 'Registra data que houve alguma atualização do objeto',
+    example: '2024-01-24T23:00:56.481Z',
+  })
+  updatedAt: Date;
+}
