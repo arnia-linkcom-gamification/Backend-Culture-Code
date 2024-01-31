2af8e9a3c507c161ede47cbdf73dd2454b063441
Glasielle <gla.cirilo@gmail.com>
2024-01-30 22:02:25 -0300

    feature: include route to find jewel by id

--- src/jewels/jewels.controller.ts
+++ src/jewels/jewels.controller.ts
@@ -11,18 +11,23 @@ import {
 import { JewelsService } from './jewels.service';
 import { CreateJewelDto } from './dto/create-jewel.dto';
 //import { UpdateJewelDto } from './dto/update-jewel.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Roles } from 'src/decorators/role.decorator';
 import { AuthGuard } from 'src/auth/guards/auth-guard';
 import { RoleEnum } from 'src/enums/role.enum';
 import { RolesGuard } from 'src/auth/guards/roles-guard';
+import { CreateJewelDoc } from './docs/create-jewel.doc';
+import { ResponseCreateJewelDoc } from './docs/response-create-jewel.doc';
 @ApiTags('4 - Joias')
 @Controller('jewels')
 export class JewelsController {
   constructor(private readonly jewelsService: JewelsService) {}
+  @Post()
+  // @ApiBody({ type: CreateJewelDoc })
+  // @ApiResponse({ type: ResponseCreateJewelDoc })
+  // @ApiBearerAuth()
   @UseGuards(AuthGuard, RolesGuard)
   @Roles(RoleEnum.admin)
-  @Post()
   create(@Body() createJewelDto: CreateJewelDto) {
     return this.jewelsService.create(createJewelDto);
   }
@@ -33,6 +38,8 @@ export class JewelsController {
   }
 
   @Get(':id')
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard)
   findOne(@Param('id') id: string) {
     return this.jewelsService.findOne(+id);
   }
