b145021cab748e6103737a9225bf5cc81f82f445
Glasielle <gla.cirilo@gmail.com>
2024-02-07 14:14:52 -0300

    test: service login

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,39 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { AuthService } from './auth.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import * as bcrypt from 'bcrypt';
+import { userRepositoryMock } from '../testing/users/user-repository.mock';
+import { usersServiceMock } from '../testing/users/users-service.mock';
+import { jwtServiceMock } from '../testing/auth/jwt-service.mock';
+import { tokenMock } from '../testing/auth/token.mock';
+import { userLoginCredentialsMock } from '../testing/auth/user-login-credentials.mock';
 
-// describe('AuthService', () => {
-//   let service: AuthService;
+describe('AuthService', () => {
+  let service: AuthService;
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [AuthService],
-//     }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        jwtServiceMock,
+        usersServiceMock,
+        userRepositoryMock,
+      ],
+    }).compile();
 
-//     service = module.get<AuthService>(AuthService);
-//   });
+    service = module.get<AuthService>(AuthService);
+  });
 
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('Should return a token', async () => {
+      jest.spyOn(bcrypt, 'compare').mockResolvedValue(true as never);
+
+      const result = await service.login(userLoginCredentialsMock);
+
+      expect(result).toEqual(tokenMock);
+    });
+  });
+});
---
+++ src/testing/auth/user-login-credentials.mock.ts
@@ -0,0 +1,4 @@
+export const userLoginCredentialsMock = {
+  email: 'glasi@silva.com.br',
+  password: 'Js@Nestjs2024',
+};
